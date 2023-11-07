fd4fdd8185ebb4cda589be95489406a1c4906d82
Elvis Chican <140889382+ElvisChican@users.noreply.github.com>
2023-11-07 20:06:09 +0200

    Update main.py
    Changes were dropped

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def modified_print_hi(name):
+def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
